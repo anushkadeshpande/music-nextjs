a0f666936b3002f54f55026b4ef00e7573c6a63d
anushkadeshpande <anushkaa5000@gmail.com>
2024-03-25 00:39:41 +0530

    feat: Added options to NavBar component and styled it

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
 
-const NavBar = () => {
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+const NavBar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
   return (
-    <h1>NavBar</h1>
-  )
-}
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      NavBar
+      <Menu setActive={setActive}>
+        <Link href="/">
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
